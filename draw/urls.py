e24694d3efa724c11c141ce7c2864fb84ca6e244
Hoteison <alexlauluo@berkeley.edu>
2021-07-24 17:36:45 -0700

    Fix image_board url

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('image_board', views.image_board, name="image_board"),
+    path('image_board/', views.image_board, name="image_board"),
     path('<str:room_name>/', views.room, name='room')
 ]
