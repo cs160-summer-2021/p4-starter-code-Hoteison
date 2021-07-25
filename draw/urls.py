030057f9b968dddfa50a3c2fd30f501cc03b687f
Hoteison <alexlauluo@berkeley.edu>
2021-07-24 17:27:30 -0700

    Create Room, Create forms

--- draw/urls.py
+++ draw/urls.py
@@ -5,6 +5,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:room_name>/', views.room, name='room'),
-
+    path('image_board', views.image_board, name="image_board"),
+    path('<str:room_name>/', views.room, name='room')
 ]
--- draw/views.py
+++ draw/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'draw/index.html')
 
+def image_board(request):
+    return render(request, 'draw/image_board.html')
+
 def room(request, room_name):
     return render(request, 'draw/room.html', {
         'room_name': room_name
